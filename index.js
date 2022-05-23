217992030a8f29241e29444075782152fae35817
ColinMVan <colin_vanhoveln@outlook.com>
2022-05-22 22:48:28 -0400

    made the menu close when about is clicked in the nav bar

--- index.js
+++ index.js
@@ -10,11 +10,16 @@ openMenu.addEventListener("click", () => {
   links.style.left = "0";
 });
 
-closeMenu.addEventListener("click", () => {
+function closesMenu() {
   links.style.left = "-100%";
   closeMenu.style.display = "none";
   openMenu.style.display = "block";
-});
+}
+
+closeMenu.addEventListener("click", closesMenu);
+
+const aboutMeLink = document.querySelector("#aboutLink");
+aboutMeLink.addEventListener("click", closesMenu);
 
 // Changes the nav bar background color when scrolling
 const navBar = document.querySelector(".nav-bar");
